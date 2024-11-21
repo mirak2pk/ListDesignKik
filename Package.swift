d2ab38e7890d1e690299a8b23a698b66fd27354e
abdelkarime achtaou <karime.parkour@gmail.com>
2024-11-21 13:52:25 +0100

    ListDesignKit

--- Package.swift
+++ Package.swift
@@ -4,18 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "ListDesignKik",
+    name: "ListDesignKit",
+    platforms: [
+        .iOS(.v13),
+        .macCatalyst(.v13),
+        .macOS(.v10_15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "ListDesignKik",
-            targets: ["ListDesignKik"]),
+            name: "ListDesignKit",
+            targets: ["ListDesignKit"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ListDesignKik"),
-
+            name: "ListDesignKit",
+            dependencies: [])
     ]
 )
